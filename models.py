2666b3a2a23712ec6194b0381e4bde557b40a2ed
Vishwanath Soratur <138495983+Vishwasoratur@users.noreply.github.com>
2025-07-13 01:43:36 +0530

    Update models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+# models.py
+from database import db
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
